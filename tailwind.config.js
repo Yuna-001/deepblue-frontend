457ad2e6c9e5a69f9fb73b4b170642b70d85353c
Lee Yuna <leeyuna7689@gmail.com>
2024-10-01 17:06:49 +0900

    chore: main 태그의 스크롤 바를 숨기기 위한 사용자 정의 스타일 추가
    - @layer components를 사용하여 main 태그의 스크롤 바를 숨기기 위한 커스텀 CSS 추가
    - tailwind.config.js 수정 및 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      navy: {
-        100: "#D6E5F4",
-        200: "#ADCCE9",
-        300: "#85B2DD",
-        500: "#337FC7",
-        800: "#143350",
-      },
-      sky_blue: {
-        primary: {
-          100: "#DFF3FB",
-          300: "#9FDCF3",
-          500: "#60C5EB",
-          600: "#4D9DBC",
-          800: "#264F5E",
-        },
-      },
-      point_color: {
-        emerald: "#00B5AE",
-        yellow: "#FFD602",
-      },
-      error_color: "#FF5602",
-      "gradient-custom": "linear-gradient(to right, #264F5E, #A5D8E7)",
-    },
     extend: {
-      backgroundImage: {
-        "gradient-custom": "linear-gradient(to right,#4D9DBC,#60C4EB, #9FDCF3)",
+      width: {
+        basic: "360px",
       },
+      height: {
+        basic: "640px",
+      },
+      minWidth: {
+        basic: "360px",
+      },
+      minHeight: {
+        basic: "640px",
+      },
+      colors: {
+        navy: {
+          100: "#D6E5F4",
+          200: "#ADCCE9",
+          300: "#85B2DD",
+          500: "#337FC7",
+          800: "#143350",
+        },
+        sky_blue: {
+          primary: {
+            100: "#DFF3FB",
+            300: "#9FDCF3",
+            500: "#60C5EB",
+            600: "#4D9DBC",
+            800: "#264F5E",
+          },
+        },
+        point_color: {
+          emerald: "#00B5AE",
+          yellow: "#FFD602",
+        },
+        error_color: "#FF5602",
+      },
+    },
+    backgroundImage: {
+      "gradient-custom": "linear-gradient(to right,#4D9DBC,#60C4EB, #9FDCF3)",
     },
   },
+
   plugins: [],
 };
