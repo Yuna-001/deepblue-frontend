58a9fa15e58d5ef89eb81d4cde351d369d9a63c0
Lee Yuna <leeyuna7689@gmail.com>
2024-10-01 19:38:37 +0900

    feat: QuestionPage에 답변 클릭 시 점수를 업데이트하는 로직 추가

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,25 +1,32 @@
 import { FC, useState } from "react";
-import questionsData from "../assets/questions.json";
 import { useNavigate } from "react-router-dom";
+
+import questionsData from "../assets/questions.json";
+import useUserStore from "../store/user";
 
 const QuestionPage: FC = () => {
   const navigate = useNavigate();
+
+  const increaseScore = useUserStore((state) => state.increaseScore);
+
   const [questionIndex, setQuestionIndex] = useState<number>(0);
   const { question, answers } = questionsData[questionIndex];
 
-  const handelAnswerClick = (score: number) => {
+  const handelAnswerClick = (point: number) => {
+    increaseScore(point);
+
     if (questionIndex === questionsData.length - 1) {
       navigate("/tutorial/nickname");
-    } else {
-      setQuestionIndex((prevIndex) => prevIndex + 1);
     }
+
+    setQuestionIndex((prevIndex) => prevIndex + 1);
   };
 
   return (
     <div>
       <h2>{question}</h2>
       <ul>
-        {answers.map((answer, score) => (
+        {answers.map((answer, point) => (
           <li key={answer} onClick={() => handelAnswerClick(score)}>
             {answer}
           </li>
