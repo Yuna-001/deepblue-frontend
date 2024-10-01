a2f16857ca3775eeee5c9f087c0217f831f984b3
Lee Yuna <leeyuna7689@gmail.com>
2024-10-01 22:18:57 +0900

    feat: 스플래시 기능 구현

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import SplashPage from "../../pages/SplashPage";
+
+const SPLASH_DISPLAY_TIME = 1500;
 
 const RootLayout: FC = () => {
+  const [isRevisit, setIsRevisit] = useState<boolean>(
+    sessionStorage.getItem("isRevisit") === "true",
+  );
+
+  useEffect(() => {
+    if (!isRevisit) {
+      const timer = setTimeout(() => {
+        sessionStorage.setItem("isRevisit", "true");
+        setIsRevisit(true);
+      }, SPLASH_DISPLAY_TIME);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isRevisit]);
+
   return (
     <div className="w-1wh h-lvh bg-navy-800 overflow-hidden flex justify-center items-center">
       <main className="w-full max-w-max_width h-full max-h-max_height bg-white overflow-y-scroll">
-        <Outlet />
+        {isRevisit ? <Outlet /> : <SplashPage />}
       </main>
     </div>
   );
