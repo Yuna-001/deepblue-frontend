af6ffa201ef28dcab1901ae599efcc8a6451fe8a
Lee Yuna <leeyuna7689@gmail.com>
2024-10-03 00:16:56 +0900

    feat: Answer 컴포넌트 추가 및 QuestionPage 스타일링
    - QuestionPage 스타일링 (로고 빼고 완료)
    - questions.json의 질문에 '\n' 이스케이프 문자 추가
    - Answer 컴포넌트 추가
    - handleAnserClick 함수 인수를 인덱스에서 인덱스+1로 변경

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -2,12 +2,14 @@ import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import questionsData from "../assets/questions.json";
-import useUserStore from "../store/user";
+import useHikikomoriScore from "../store/hikikomoriScore";
+import Answer from "../components/buttons/Answer";
+import PageLayout from "../components/layout/PageLayout";
 
 const QuestionPage: FC = () => {
   const navigate = useNavigate();
 
-  const increaseScore = useUserStore((state) => state.increaseScore);
+  const increaseScore = useHikikomoriScore((state) => state.increaseScore);
 
   const [questionIndex, setQuestionIndex] = useState<number>(0);
   const { question, answers } = questionsData[questionIndex];
@@ -16,23 +18,31 @@ const QuestionPage: FC = () => {
     increaseScore(point);
 
     if (questionIndex === questionsData.length - 1) {
-      navigate("/tutorial/nickname");
+      // 백엔드로 전송
+      navigate("/tutorial/nickname-setting");
     }
 
     setQuestionIndex((prevIndex) => prevIndex + 1);
   };
 
   return (
-    <div>
-      <h2>{question}</h2>
-      <ul>
+    <PageLayout withBackArrow>
+      <div className="w-full flex flex-col items-center gap-7 pt-7">
+        <div className="w-20 h-20 bg-slate-100">로고 대신</div>
+        <h2 className="title3 h-16 py-auto text-navy-100 whitespace-pre-line text-center">
+          {question}
+        </h2>
+      </div>
+      <ul className="w-full flex flex-col gap-3 my-5">
         {answers.map((answer, point) => (
-          <li key={answer} onClick={() => handelAnswerClick(score)}>
-            {answer}
-          </li>
+          <Answer
+            key={answer}
+            text={answer}
+            onClick={() => handelAnswerClick(point + 1)}
+          />
         ))}
       </ul>
-    </div>
+    </PageLayout>
   );
 };
 
