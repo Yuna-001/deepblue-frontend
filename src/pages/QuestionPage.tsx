0de97309426080182e66515fd562a8817466e57c
Lee Yuna <leeyuna7689@gmail.com>
2024-10-04 16:56:53 +0900

    feat: QuestionPage에 로고 추가

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -5,8 +5,14 @@ import questionsData from "../assets/questions.json";
 import Answer from "../components/buttons/Answer";
 import PageLayout from "../components/layout/PageLayout";
 
+import question1Img from "../assets/images/onboarding/question1.svg";
+import question2Img from "../assets/images/onboarding/question2.svg";
+import question3Img from "../assets/images/onboarding/question3.svg";
+
 const QuestionPage: FC = () => {
   const navigate = useNavigate();
+
+  const imgList = [question1Img, question2Img, question3Img];
 
   const [questionIndex, setQuestionIndex] = useState<number>(0);
   const [hikikomoriScore, setHikikomoriScore] = useState<number>(0);
@@ -30,12 +36,12 @@ const QuestionPage: FC = () => {
   return (
     <PageLayout withBackArrow>
       <div className="w-full flex flex-col items-center gap-7 pt-7">
-        <div className="w-20 h-20 bg-slate-100">로고 대신</div>
+        <img src={imgList[questionIndex]} />
         <h2 className="title3 h-16 py-auto text-navy-100 whitespace-pre-line text-center">
           {question}
         </h2>
       </div>
-      <ul className="w-full flex flex-col gap-3 my-5">
+      <ul className="w-full flex flex-col gap-3 mt-3">
         {answers.map((answer, point) => (
           <Answer
             key={answer}
