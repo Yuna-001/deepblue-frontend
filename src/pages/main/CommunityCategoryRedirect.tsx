145b7b397a92804a11da6a1f9e7fb042ec3e2fec
Lee Yuna <leeyuna7689@gmail.com>
2024-12-22 02:19:07 +0900

    fix: 유효하지 않은 커뮤니티 카테고리 리다이렉션 추가
    - 유효하지 않은 카테고리(free, worry, career 이외`)에 대해 리다이렉션 처리 추가

--- src/pages/main/CommunityCategoryRedirect.tsx
+++ src/pages/main/CommunityCategoryRedirect.tsx
@@ -4,6 +4,10 @@ import { Navigate, useParams } from "react-router-dom";
 const CommunityCategoryRedirect: FC = () => {
   const { category } = useParams();
 
+  if (category !== "free" && category !== "worry" && category !== "career") {
+    return <Navigate to={`/main/communication/all/realtime`} />;
+  }
+
   return <Navigate to={`/main/communication/${category}/realtime`} />;
 };
 
