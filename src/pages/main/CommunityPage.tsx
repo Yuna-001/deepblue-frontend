65037d4b33b63852e7bd7e1475f514ac922c0f8f
Lee Yuna <leeyuna7689@gmail.com>
2024-12-27 18:46:02 +0900

    refactor: CommunityPage에서 CommunityHeader 분할

--- src/pages/main/CommunityPage.tsx
+++ src/pages/main/CommunityPage.tsx
@@ -2,23 +2,15 @@ import { useParams } from "react-router-dom";
 
 import MainPageLayout from "../../components/layout/MainPageLayout";
 import PostList from "../../components/community/PostList";
-import CategoryNavigation from "../../components/community/CategoryNavigation";
 import SortNavigation from "../../components/community/SortNavigation";
-import MainHeader from "../../components/layout/MainHeader";
-import searchIcon from "../../assets/images/community/search.svg";
+import CommunityHeader from "../../components/community/CommuniityHeader";
 
 const CommunityPage = () => {
   const { category, sort } = useParams();
 
   return (
     <>
-      <div>
-        <MainHeader title="커뮤니티" additionalClass="pl-4">
-          <img src={searchIcon} className="mr-1" />
-        </MainHeader>
-        <CategoryNavigation sort={sort} />
-      </div>
-
+      <CommunityHeader sort={sort} />
       <MainPageLayout>
         <div className="h-full overflow-y-auto scrollbar-hidden pb-16">
           <div className="pb-16">
